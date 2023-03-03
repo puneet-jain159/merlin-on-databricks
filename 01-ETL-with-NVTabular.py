7af6d5e163eaaa30716adf3a4135544eeac3fa93
puneet.jain@databricks.com <puneet.jain@databricks.com>
2023-03-03 14:55:21 +0000

    Adding end-to-end session example for transformer4rec
    This is working on 11.3 LTS

---
+++ 01-ETL-with-NVTabular.py
@@ -0,0 +1,421 @@
+# Databricks notebook source
+# Copyright 2022 NVIDIA Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# Each user is responsible for checking the content of datasets and the
+# applicable licenses and determining if suitable for the intended use.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <img src="https://developer.download.nvidia.com/notebooks/dlsw-notebooks/merlin_transformers4rec_end-to-end-session-based-01-etl-with-nvtabular/nvidia_logo.png" style="width: 90px; float: right;">
+# MAGIC 
+# MAGIC # ETL with NVTabular
+# MAGIC 
+# MAGIC This notebook is created using the latest stable [merlin-pytorch](https://catalog.ngc.nvidia.com/orgs/nvidia/teams/merlin/containers/merlin-pytorch) container.
+# MAGIC 
+# MAGIC **Start a GPU CLuster and run the below magic commmand**
+# MAGIC ```
+# MAGIC %pip install -r requirements.txt --extra-index-url=https://pypi.nvidia.com
+# MAGIC ```
+# MAGIC This script will mount your local data folder that includes your data files to `/workspace/data` directory in the merlin-pytorch docker container.
+
+# COMMAND ----------
+
+# MAGIC %pip install -r requirements.txt --extra-index-url=https://pypi.nvidia.com
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Overview
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This notebook demonstrates how to use NVTabular to perform the feature engineering that is needed to model the `YOOCHOOSE` dataset which contains a collection of sessions from a retailer. Each session  encapsulates the click events that the user performed in that session.
+# MAGIC 
+# MAGIC The dataset is available on [Kaggle](https://www.kaggle.com/chadgostopp/recsys-challenge-2015). You need to download it and copy to the `DATA_FOLDER` path. Note that we are only using the `yoochoose-clicks.dat` file.
+# MAGIC 
+# MAGIC First, let's start by importing several libraries:
+
+# COMMAND ----------
+
+import os
+import glob
+import numpy as np
+import gc
+
+import cudf
+import cupy
+import nvtabular as nvt
+from merlin.dag import ColumnSelector
+from merlin.schema import Schema, Tags
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Define Data Input and Output Paths
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ```
+# MAGIC %sh 
+# MAGIC pip install kaggle
+# MAGIC export KAGGLE_USERNAME=puneetjain159
+# MAGIC export KAGGLE_KEY=dfac4e000065134f65640b201645dab9
+# MAGIC kaggle datasets download -d chadgostopp/recsys-challenge-2015 -p /dbfs/merlin/data/
+# MAGIC cd /dbfs/merlin/data/ &&  unzip /dbfs/merlin/data/recsys-challenge-2015.zip 
+# MAGIC ```
+
+# COMMAND ----------
+
+### Download Data 
+
+# COMMAND ----------
+
+import os
+import glob
+import numpy as np
+import gc
+
+import cudf
+import cupy
+import nvtabular as nvt
+from merlin.dag import ColumnSelector
+from merlin.schema import Schema, Tags
+
+# COMMAND ----------
+
+import os 
+
+DATA_FOLDER = "/dbfs/merlin/data/"
+FILENAME_PATTERN = 'yoochoose-clicks.dat'
+DATA_PATH = os.path.join(DATA_FOLDER, FILENAME_PATTERN)
+
+OUTPUT_FOLDER  = os.path.join("/dbfs/merlin/data/","output/yoochoose_transformed")
+OVERWRITE = False
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load and clean raw data
+
+# COMMAND ----------
+
+interactions_df = cudf.read_csv(DATA_PATH, sep=',', 
+                                names=['session_id','timestamp', 'item_id', 'category'], 
+                                dtype=['int', 'datetime64[s]', 'int', 'int'])
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Remove repeated interactions within the same session
+
+# COMMAND ----------
+
+print("Count with in-session repeated interactions: {}".format(len(interactions_df)))
+# Sorts the dataframe by session and timestamp, to remove consecutive repetitions
+interactions_df.timestamp = interactions_df.timestamp.astype(int)
+interactions_df = interactions_df.sort_values(['session_id', 'timestamp'])
+past_ids = interactions_df['item_id'].shift(1).fillna()
+session_past_ids = interactions_df['session_id'].shift(1).fillna()
+# Keeping only no consecutive repeated in session interactions
+interactions_df = interactions_df[~((interactions_df['session_id'] == session_past_ids) & (interactions_df['item_id'] == past_ids))]
+print("Count after removed in-session repeated interactions: {}".format(len(interactions_df)))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Create new feature with the timestamp when the item was first seen
+
+# COMMAND ----------
+
+items_first_ts_df = interactions_df.groupby('item_id').agg({'timestamp': 'min'}).reset_index().rename(columns={'timestamp': 'itemid_ts_first'})
+interactions_merged_df = interactions_df.merge(items_first_ts_df, on=['item_id'], how='left')
+interactions_merged_df.head()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Let's save the interactions_merged_df to disk to be able to use in the inference step.
+
+# COMMAND ----------
+
+interactions_merged_df.to_parquet(os.path.join(DATA_FOLDER, 'interactions_merged_df.parquet'))
+
+# COMMAND ----------
+
+# free gpu memory
+del interactions_df, session_past_ids, items_first_ts_df
+gc.collect()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##  Define a preprocessing workflow with NVTabular
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC NVTabular is a feature engineering and preprocessing library for tabular data designed to quickly and easily manipulate terabyte scale datasets used to train deep learning based recommender systems. It provides a high level abstraction to simplify code and accelerates computation on the GPU using the RAPIDS cuDF library.
+# MAGIC 
+# MAGIC NVTabular supports different feature engineering transformations required by deep learning (DL) models such as Categorical encoding and numerical feature normalization. It also supports feature engineering and generating sequential features. 
+# MAGIC 
+# MAGIC More information about the supported features can be found <a href="https://nvidia-merlin.github.io/NVTabular/main/index.html> here. </a">
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Feature engineering: Create and Transform items features
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In this cell, we are defining three transformations ops: 
+# MAGIC 
+# MAGIC - 1. Encoding categorical variables using `Categorify()` op. We set `start_index` to 1 so that encoded null values start from `1` instead of `0` because we reserve `0` for padding the sequence features.
+# MAGIC - 2. Deriving temporal features from timestamp and computing their cyclical representation using a custom lambda function. 
+# MAGIC - 3. Computing the item recency in days using a custom op. Note that item recency is defined as the difference between the first occurrence of the item in dataset and the actual date of item interaction. 
+# MAGIC 
+# MAGIC For more ETL workflow examples, visit NVTabular [example notebooks](https://github.com/NVIDIA-Merlin/NVTabular/tree/main/examples).
+
+# COMMAND ----------
+
+# Encodes categorical features as contiguous integers
+cat_feats = ColumnSelector(['session_id', 'category', 'item_id']) >> nvt.ops.Categorify(start_index=1)
+
+# create time features
+session_ts = ColumnSelector(['timestamp'])
+session_time = (
+    session_ts >> 
+    nvt.ops.LambdaOp(lambda col: cudf.to_datetime(col, unit='s')) >> 
+    nvt.ops.Rename(name = 'event_time_dt')
+)
+sessiontime_weekday = (
+    session_time >> 
+    nvt.ops.LambdaOp(lambda col: col.dt.weekday) >> 
+    nvt.ops.Rename(name ='et_dayofweek')
+)
+
+# Derive cyclical features: Define a custom lambda function 
+def get_cycled_feature_value_sin(col, max_value):
+    value_scaled = (col + 0.000001) / max_value
+    value_sin = np.sin(2*np.pi*value_scaled)
+    return value_sin
+
+weekday_sin = sessiontime_weekday >> (lambda col: get_cycled_feature_value_sin(col+1, 7)) >> nvt.ops.Rename(name = 'et_dayofweek_sin')
+
+# Compute Item recency: Define a custom Op 
+class ItemRecency(nvt.ops.Operator):
+    def transform(self, columns, gdf):
+        for column in columns.names:
+            col = gdf[column]
+            item_first_timestamp = gdf['itemid_ts_first']
+            delta_days = (col - item_first_timestamp) / (60*60*24)
+            gdf[column + "_age_days"] = delta_days * (delta_days >=0)
+        return gdf
+
+    def compute_selector(
+        self,
+        input_schema: Schema,
+        selector: ColumnSelector,
+        parents_selector: ColumnSelector,
+        dependencies_selector: ColumnSelector,
+    ) -> ColumnSelector:
+        self._validate_matching_cols(input_schema, parents_selector, "computing input selector")
+        return parents_selector
+
+    def column_mapping(self, col_selector):
+        column_mapping = {}
+        for col_name in col_selector.names:
+            column_mapping[col_name + "_age_days"] = [col_name]
+        return column_mapping
+
+    @property
+    def dependencies(self):
+        return ["itemid_ts_first"]
+
+    @property
+    def output_dtype(self):
+        return np.float64
+    
+recency_features = session_ts >> ItemRecency() 
+# Apply standardization to this continuous feature
+recency_features_norm = recency_features >> nvt.ops.LogOp() >> nvt.ops.Normalize(out_dtype=np.float32) >> nvt.ops.Rename(name='product_recency_days_log_norm')
+
+time_features = (
+    session_time +
+    sessiontime_weekday +
+    weekday_sin + 
+    recency_features_norm
+)
+
+features = ColumnSelector(['timestamp', 'session_id']) + cat_feats + time_features 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Define the preprocessing of sequential features
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Once the item features are generated, the objective of this cell is to group interactions at the session level, sorting the interactions by time. We additionally truncate all sessions to first 20 interactions and filter out sessions with less than 2 interactions.
+
+# COMMAND ----------
+
+# Define Groupby Operator
+groupby_features = features >> nvt.ops.Groupby(
+    groupby_cols=["session_id"], 
+    sort_cols=["timestamp"],
+    aggs={
+        'item_id': ["list", "count"],
+        'category': ["list"],  
+        'timestamp': ["first"],
+        'event_time_dt': ["first"],
+        'et_dayofweek_sin': ["list"],
+        'product_recency_days_log_norm': ["list"]
+        },
+    name_sep="-") >> nvt.ops.AddMetadata(tags=[Tags.CATEGORICAL])
+
+
+# Truncate sequence features to first interacted 20 items 
+SESSIONS_MAX_LENGTH = 20 
+
+groupby_features_list = groupby_features['item_id-list', 'category-list', 'et_dayofweek_sin-list', 'product_recency_days_log_norm-list']
+groupby_features_truncated = groupby_features_list >> nvt.ops.ListSlice(0, SESSIONS_MAX_LENGTH, pad=True) >> nvt.ops.Rename(postfix = '_seq')
+
+# Calculate session day index based on 'event_time_dt-first' column
+day_index = ((groupby_features['event_time_dt-first'])  >> 
+    nvt.ops.LambdaOp(lambda col: (col - col.min()).dt.days +1) >> 
+    nvt.ops.Rename(f = lambda col: "day_index")
+)
+
+# Select features for training 
+selected_features = groupby_features['session_id', 'item_id-count'] + groupby_features_truncated + day_index
+
+# Filter out sessions with less than 2 interactions 
+MINIMUM_SESSION_LENGTH = 2
+filtered_sessions = selected_features >> nvt.ops.Filter(f=lambda df: df["item_id-count"] >= MINIMUM_SESSION_LENGTH) 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Avoid Numba low occupancy warnings:
+
+# COMMAND ----------
+
+from numba import config
+config.CUDA_LOW_OCCUPANCY_WARNINGS = 0
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Execute NVTabular workflow
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Once we have defined the general workflow (`filtered_sessions`), we provide our cudf dataset to `nvt.Dataset` class which is optimized to split data into chunks that can fit in device memory and to handle the calculation of complex global statistics. Then, we execute the pipeline that fits and transforms data to get the desired output features.
+
+# COMMAND ----------
+
+dataset = nvt.Dataset(interactions_merged_df)
+workflow = nvt.Workflow(filtered_sessions)
+# Learn features statistics necessary of the preprocessing workflow
+workflow.fit(dataset)
+# Apply the preprocessing workflow in the dataset and convert the resulting Dask cudf dataframe to a cudf dataframe
+sessions_gdf = workflow.transform(dataset).compute()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Let's print the head of our preprocessed dataset. You can notice that now each example (row) is a session and the sequential features with respect to user interactions were converted to lists with matching length.
+
+# COMMAND ----------
+
+sessions_gdf.head()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Save the preprocessing workflow
+
+# COMMAND ----------
+
+workflow.save(os.path.join("/dbfs/merlin/workflow/","workflow_etl"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Export pre-processed data by day
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In this example we are going to split the preprocessed parquet files by days, to allow for temporal training and evaluation. There will be a folder for each day and three parquet files within each day: `train.parquet`, `validation.parquet` and `test.parquet`.
+# MAGIC   
+# MAGIC P.s. It is worthwhile to note that the dataset has a single categorical feature (category), which, however, is inconsistent over time in the dataset. All interactions before day 84 (2014-06-23) have the same value for that feature, whereas many other categories are introduced afterwards. Thus for this example, we save only the last five days.
+
+# COMMAND ----------
+
+sessions_gdf = sessions_gdf[sessions_gdf.day_index>=178]
+
+# COMMAND ----------
+
+from utils.date_utils import save_time_based_splits
+
+# COMMAND ----------
+
+# from utils.data_utils import save_time_based_splits
+save_time_based_splits(data=nvt.Dataset(sessions_gdf),
+                       output_dir= os.path.join("/dbfs/merlin/workflow/","output/preproc_sessions_by_day"),
+                       partition_col='day_index',
+                       timestamp_col='session_id', 
+                      )
+
+# COMMAND ----------
+
+def list_files(startpath):
+    """
+    Util function to print the nested structure of a directory
+    """
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, "").count(os.sep)
+        indent = " " * 4 * (level)
+        print("{}{}/".format(indent, os.path.basename(root)))
+        subindent = " " * 4 * (level + 1)
+        for f in files:
+            print("{}{}".format(subindent, f))
+
+# COMMAND ----------
+
+list_files(os.path.join("/dbfs/merlin/workflow/","output/preproc_sessions_by_day"))
+
+# COMMAND ----------
+
+# free gpu memory
+del  sessions_gdf
+gc.collect()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC That's it! We created our sequential features, now we can go to the next notebook to train a PyTorch session-based model.
+
+# COMMAND ----------
+
+
---
+++ 02-End-to-end-session-based-with-Yoochoose-PyT.py
@@ -0,0 +1,251 @@
+# Databricks notebook source
+# Copyright 2022 NVIDIA Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# Each user is responsible for checking the content of datasets and the
+# applicable licenses and determining if suitable for the intended use.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <img src="https://developer.download.nvidia.com/notebooks/dlsw-notebooks/merlin_transformers4rec_end-to-end-session-based-02-end-to-end-session-based-with-yoochoose-pyt/nvidia_logo.png" style="width: 90px; float: right;">
+# MAGIC 
+# MAGIC # End-to-end session-based recommendations with PyTorch
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Start a GPU CLuster and run the below magic commmand**
+# MAGIC ```
+# MAGIC %pip install -r requirements.txt --extra-index-url=https://pypi.nvidia.com
+# MAGIC ```
+
+# COMMAND ----------
+
+# MAGIC %pip install -r requirements.txt --extra-index-url=https://pypi.nvidia.com
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In recent years, several deep learning-based algorithms have been proposed for recommendation systems while its adoption in industry deployments have been steeply growing. In particular, NLP inspired approaches have been successfully adapted for sequential and session-based recommendation problems, which are important for many domains like e-commerce, news and streaming media. Session-Based Recommender Systems (SBRS) have been proposed to model the sequence of interactions within the current user session, where a session is a short sequence of user interactions typically bounded by user inactivity. They have recently gained popularity due to their ability to capture short-term or contextual user preferences towards items. 
+# MAGIC 
+# MAGIC The field of NLP has evolved significantly within the last decade, particularly due to the increased usage of deep learning. As a result, state of the art NLP approaches have inspired RecSys practitioners and researchers to adapt those architectures, especially for sequential and session-based recommendation problems. Here, we leverage one of the state-of-the-art Transformer-based architecture, [XLNet](https://arxiv.org/abs/1906.08237) with Masked Language Modeling (MLM) training technique (see our [tutorial](https://github.com/NVIDIA-Merlin/Transformers4Rec/tree/main/examples/tutorial) for details) for training a session-based model.
+# MAGIC 
+# MAGIC In this end-to-end-session-based recommnender model example, we use `Transformers4Rec` library, which leverages the popular [HuggingFace’s Transformers](https://github.com/huggingface/transformers) NLP library and make it possible to experiment with cutting-edge implementation of such architectures for sequential and session-based recommendation problems. For detailed explanations of the building blocks of Transformers4Rec meta-architecture visit [getting-started-session-based](https://github.com/NVIDIA-Merlin/Transformers4Rec/tree/main/examples/getting-started-session-based) and [tutorial](https://github.com/NVIDIA-Merlin/Transformers4Rec/tree/main/examples/tutorial) example notebooks.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 1. Model definition using Transformers4Rec
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In the previous notebook, we have created sequential features and saved our processed data frames as parquet files. Now we use these processed parquet files to train a session-based recommendation model with the XLNet architecture.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 1.1 Get the schema 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC The library uses a schema format to configure the input features and automatically creates the necessary layers. This *protobuf* text file contains the description of each input feature by defining: the name, the type, the number of elements of a list column,  the cardinality of a categorical feature and the min and max values of each feature. In addition, the annotation field contains the tags such as specifying the `continuous` and `categorical` features, the `target` column or the `item_id` feature, among others.
+
+# COMMAND ----------
+
+from utils.merlin_utils import Schema
+SCHEMA_PATH = "schema_demo.pb"
+schema = Schema().from_proto_text(SCHEMA_PATH)
+!cat $SCHEMA_PATH
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We can select the subset of features we want to use for training the model by their tags or their names.
+
+# COMMAND ----------
+
+schema = schema.select_by_name(
+   ['item_id-list_seq', 'category-list_seq', 'product_recency_days_log_norm-list_seq', 'et_dayofweek_sin-list_seq']
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 1.2 Define the end-to-end Session-based Transformer-based recommendation model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC For defining a session-based recommendation model, the end-to-end model definition requires four steps:
+# MAGIC 
+# MAGIC 1. Instantiate [TabularSequenceFeatures](https://nvidia-merlin.github.io/Transformers4Rec/main/api/transformers4rec.tf.features.html?highlight=tabularsequence#transformers4rec.tf.features.sequence.TabularSequenceFeatures) input-module from schema to prepare the embedding tables of categorical variables and project continuous features, if specified. In addition, the module provides different aggregation methods (e.g. 'concat', 'elementwise-sum') to merge input features and generate the sequence of interactions embeddings. The module also supports language modeling tasks to prepare masked labels for training and evaluation (e.g: 'mlm' for masked language modeling) 
+# MAGIC 
+# MAGIC 2. Next, we need to define one or multiple prediction tasks. For this demo, we are going to use [NextItemPredictionTask](https://nvidia-merlin.github.io/Transformers4Rec/main/api/transformers4rec.tf.model.html?highlight=nextitem#transformers4rec.tf.model.prediction_task.NextItemPredictionTask) with `Masked Language modeling`: during training, randomly selected items are masked and predicted using the unmasked sequence items. For inference, it is meant to always predict the next item to be interacted with.
+# MAGIC 
+# MAGIC 3. Then we construct a `transformer_config` based on the architectures provided by [Hugging Face Transformers](https://github.com/huggingface/transformers) framework. </a>
+# MAGIC 
+# MAGIC 4. Finally we link the transformer-body to the inputs and the prediction tasks to get the final pytorch `Model` class.
+# MAGIC     
+# MAGIC For more details about the features supported by each sub-module, please check out the library [documentation](https://nvidia-merlin.github.io/Transformers4Rec/main/index.html) page.
+
+# COMMAND ----------
+
+from transformers4rec import torch as tr
+
+max_sequence_length, d_model = 20, 320
+# Define input module to process tabular input-features and to prepare masked inputs
+input_module = tr.TabularSequenceFeatures.from_schema(
+    schema,
+    max_sequence_length=max_sequence_length,
+    continuous_projection=64,
+    aggregation="concat",
+    d_output=d_model,
+    masking="mlm",
+)
+
+# Define Next item prediction-task 
+prediction_task = tr.NextItemPredictionTask(weight_tying=True)
+
+# Define the config of the XLNet Transformer architecture
+transformer_config = tr.XLNetConfig.build(
+    d_model=d_model, n_head=8, n_layer=2, total_seq_length=max_sequence_length
+)
+
+# Get the end-to-end model 
+model = transformer_config.to_torch_model(input_module, prediction_task)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC You can print out the model structure by uncommenting the line below.
+
+# COMMAND ----------
+
+model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 1.3. Daily Fine-Tuning: Training over a time window¶
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Now that the model is defined, we are going to launch training. For that, Transfromers4rec extends HF Transformers Trainer class to adapt the evaluation loop for session-based recommendation task and the calculation of ranking metrics. The original `train()` method is not modified meaning that we leverage the efficient training implementation from that library, which manages, for example, half-precision (FP16) training.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Set the training arguments
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC An additional argument `data_loader_engine` is defined to automatically load the features needed for training using the schema. The default value is `merlin` for optimized GPU-based data-loading.  Optionally a `PyarrowDataLoader` (`pyarrow`) can also be used as a basic option, but it is slower and works only for small datasets, as the full data is loaded to CPU memory.
+
+# COMMAND ----------
+
+training_args = tr.trainer.T4RecTrainingArguments(
+            output_dir="./tmp",
+            max_sequence_length=20,
+            data_loader_engine='merlin',
+            num_train_epochs=10, 
+            dataloader_drop_last=False,
+            per_device_train_batch_size = 384,
+            per_device_eval_batch_size = 512,
+            learning_rate=0.0005,
+            fp16=True,
+            report_to = [],
+            logging_steps=200
+        )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Instantiate the trainer
+
+# COMMAND ----------
+
+recsys_trainer = tr.Trainer(
+    model=model,
+    args=training_args,
+    schema=schema,
+    compute_metrics=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Launch daily training and evaluation
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC In this demo, we will use the `fit_and_evaluate` method that allows us to conduct a time-based finetuning by iteratively training and evaluating using a sliding time window: At each iteration, we use the training data of a specific time index $t$ to train the model; then we evaluate on the validation data of the next index $t + 1$. Particularly, we set start time to 178 and end time to 180.
+
+# COMMAND ----------
+
+from transformers4rec.torch.utils.examples_utils import fit_and_evaluate
+import os
+OT_results = fit_and_evaluate(recsys_trainer, start_time_index=178, end_time_index=180, input_dir=os.path.join("/dbfs/merlin/workflow/","output/preproc_sessions_by_day"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Visualize the average of metrics over time
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC `OT_results` is a list of scores (accuracy metrics) for evaluation based on given start and end time_index. Since in this example we do evaluation on days 179, 180 and 181, we get three metrics in the list one for each day.
+
+# COMMAND ----------
+
+OT_results
+
+# COMMAND ----------
+
+import numpy as np
+# take the average of metric values over time
+avg_results = {k: np.mean(v) for k,v in OT_results.items()}
+for key in sorted(avg_results.keys()): 
+    print(" %s = %s" % (key, str(avg_results[key]))) 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Save the model
+
+# COMMAND ----------
+
+recsys_trainer._save_model_and_checkpoint(save_model_class=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## References
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Merlin Transformers4rec: https://github.com/NVIDIA-Merlin/Transformers4Rec
+# MAGIC 
+# MAGIC - Merlin NVTabular: https://github.com/NVIDIA-Merlin/NVTabular/tree/main/nvtabular
+# MAGIC 
+# MAGIC - Merlin Dataloader: https://github.com/NVIDIA-Merlin/dataloader
+# MAGIC 
+# MAGIC - Triton inference server: https://github.com/triton-inference-server
